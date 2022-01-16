4a2de9db4bfd9c0a6ac8486ff6664f290b404e52
Isabel <iarribas01@gmail.com>
2022-01-16 17:55:57 +0000

    more exercises Easy_4

--- Exercises/Easy_4/convert_num_to_str.rb
+++ Exercises/Easy_4/convert_num_to_str.rb
@@ -1,23 +1,24 @@
+
 DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 def integer_to_string(int)
   # int.digits.reverse.join
   int_str = ''
 
-  place = 1
+  place = 10
   # starting with the smallest place value
-
-  until int==0
-    digit = DIGITS[int % place * 10]
-    place *= 10
-    # take the largest place value and add to empty string
-    # subtract that value from the integer
-    int -= int % place * 10
-  # continue until the number is zero
-  # reverse the string
-  # return string
+  if int==0
+    int_str = '0'
   end
-
+  until int == 0
+    # ex) 4321
+    digit = int%place # extract num -> 4000  300  20  1
+    int -= digit # take away from num -> 4321  4320  4300  4000 0
+    digit = digit / (place/10) # take first digit of digit -> 4000 = 4
+    int_str << DIGITS[digit] # conversion -> 4 = '4'
+    place *= 10 # move to next place
+  end
+  int_str.reverse
 end
 
 
---
+++ Exercises/Easy_4/convert_signed_num_to_str.rb
@@ -0,0 +1,28 @@
+# current problem bug causing infinite loop or lag
+
+DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+def integer_to_string(number)
+  result = ''
+  loop do
+    number, remainder = number.divmod(10)
+    result.prepend(DIGITS[remainder])
+    break if number == 0
+  end
+  result
+end
+
+def signed_integer_to_string(num)
+  sign = ''
+  if num > 0
+    sign = '+'
+  else
+    sign = '-'
+  end
+
+  int_str = sign + integer_to_string(num)
+end
+
+p signed_integer_to_string(4321) == '+4321'
+p signed_integer_to_string(-123) == '-123'
+p signed_integer_to_string(0) == '0'
