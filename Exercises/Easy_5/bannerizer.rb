f378fd45c70a337d9efe232baac7325726b4bfdc
Isabel <iarribas01@gmail.com>
2022-01-23 09:36:35 +0000

    Finished bannerizer.rb, needs to be refactored

--- Exercises/Easy_5/bannerizer.rb
+++ Exercises/Easy_5/bannerizer.rb
@@ -1,8 +1,5 @@
-# Needed additions:
-# Further exploration - can wrap text when given limit
-# stuck: try drawing out steps
-# try decided on a specific data structure and how
-# that gets manipulated throughout the program
+# refactor code to be more readable
+# consistency with variable names
 
 def print_in_box(str)
   top_and_bottom = '+' + ('-'*(str.size+2)) + '+'
@@ -15,47 +12,68 @@ def print_in_box(str)
   puts top_and_bottom
 end
 
-# # returns a new string truncated to appropriate limit
-# def chop(str, limit)
-#   str[0, limit]
-# end
+def print_wrapped_in_box(str)
+  max_length = get_max_length(str)
+  top_and_bottom = '+' + ('-'*(max_length+2)) + '+'
+  middle_pad = '|' + (' '*(max_length+2)) + '|'
+  str = centerized(str)
 
-# returns an array of strings containing text
-# below limit
-def wrap(str, limit)
-  words = str.split
+  puts top_and_bottom
+  puts middle_pad
+  str.each do |row|
+    puts "| #{row} |"
+  end
+  puts middle_pad
+  puts top_and_bottom
+end
+
+def centerized(wrapped_text)
+  max_length = get_max_length(wrapped_text)
+  wrapped_text.map do |row|
+    row.center(max_length)
+  end
+end
+
+
+def get_max_length(wrapped_text)
+  max_length = 0
+  wrapped_text.each do |row| 
+    if (row.size > max_length)
+      max_length = row.size 
+    end
+  end
+  max_length
+end
+
+# returns a new string truncated to appropriate limit
+def chop(str, limit)
+  str[0, limit]
+end
+
+# returns an array of strings containing text below or equal to limit
+def wrap(message, limit)
+  words = message.split
   
   # create array
-  wrapped_text = Array.new
-  row_num = 0
-  
+  wrapped_text = Array.new # [] empty array
+  row = 0
+  wrapped_text[0] = '' # initialize first array element to avoid problems
+
   words.each do |word|
-    # initialize the new row
-    if wrapped_text[row_num] == nil
-      wrapped_text[row_num] = ''
+    if ((wrapped_text[row].size + word.size)>limit)
+      row += 1
+      wrapped_text[row] = '' # initialize array element to avoid problems
     end
-
-    # increment to next row if limit reached
-    if ((wrapped_text[row_num].size + word.size) > limit)
-      row_num += 1
-    end
-
-    # debug output
-    p "row num: #{row_num} || #{wrapped_text[row_num]}"
-
-    # add text to current row
-    wrapped_text[row_num] << word
-
-    # add space in between words
-    wrapped_text[row_num] << ' '
+    wrapped_text[row] << word
+    wrapped_text[row] << ' '
   end
+
+  wrapped_text = wrapped_text.map {|str| str.strip}
   
   wrapped_text
 end
 
-
-
-
-p wrap('To boldly go where no one has gone before.', 10)
+text = wrap('To boldly go where no one has gone before.', 30)
+puts print_wrapped_in_box(text)
 
 # print_in_box(chop('To boldly go where no one has gone before.', 20))
